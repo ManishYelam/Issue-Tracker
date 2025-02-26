63f9630b5e1273d25ee690d4bd5a32ae11b8ee24
Manish-Yelam <manish.ovtpl@gmail.com>
2025-02-26 17:40:11 +0530

    fix

--- src/Api/Models/Association.js
+++ src/Api/Models/Association.js
@@ -5,11 +5,8 @@ const Permission = require('./Permission');
 const RolePermissions = require('./RolePermissions');
 const Organization = require('./Organization');
 const ApplicationProperties = require('./Application.Prop');
-const Idea = require('./Ideas');
 const UserActions = require('./UserActions');
 const Like = require('./likes');
-const Favorite = require('./Favorites');
-const Share = require('./Shares');
 const UserConnection = require('./UserConnections');
 const Issue = require('./Issue');
 const IssueHistory = require('./IssueHistory');
@@ -29,12 +26,12 @@ Role.belongsToMany(Permission, { through: 'RolePermissions', foreignKey: 'role_i
 Permission.belongsToMany(Role, { through: 'RolePermissions', foreignKey: 'permission_id', otherKey: 'role_id' });
 
 // Organization-User relationship: An organization has many users.
-// Organization.hasMany(User, { foreignKey: 'id' });
-// User.belongsTo(Organization, { foreignKey: 'id' });
+Organization.hasMany(User, { foreignKey: 'id' });
+User.belongsTo(Organization, { foreignKey: 'id' });
 
 // Organization-Role relationship: An organization has many roles.
-// Organization.hasMany(Role, { foreignKey: 'id' });
-// Role.belongsTo(Organization, { foreignKey: 'id' });
+Organization.hasMany(Role, { foreignKey: 'id' });
+Role.belongsTo(Organization, { foreignKey: 'id' });
 
 // User-ApplicationProperties relationship: A user has many application properties.
 User.hasMany(ApplicationProperties, { foreignKey: 'id', as: 'applicationProperties' });
@@ -61,71 +58,6 @@ IssueComment.belongsTo(Issue, { foreignKey: 'issueId' });
 Issue.hasMany(IssueHistory, { foreignKey: 'issueId', as: 'history' });
 IssueHistory.belongsTo(Issue, { foreignKey: 'issueId' });
 
-// User-Idea relationship: A user can have many ideas.
-User.hasMany(Idea, { foreignKey: 'user_id' });
-Idea.belongsTo(User, { foreignKey: 'user_id' });
-
-// Organization-Idea relationship: An organization can have many ideas.
-Organization.hasMany(Idea, { foreignKey: 'organization_id' });
-Idea.belongsTo(Organization, { foreignKey: 'organization_id' });
-
-// Role-Idea many-to-many relationship via RoleIdeas.
-Role.belongsToMany(Idea, { through: 'RoleIdeas', foreignKey: 'role_id', otherKey: 'idea_id' });
-Idea.belongsToMany(Role, { through: 'RoleIdeas', foreignKey: 'idea_id', otherKey: 'role_id' });
-
-// User-UserActions relationship: A user can have many actions.
-User.hasMany(UserActions, { foreignKey: 'user_id', as: 'actions' });
-UserActions.belongsTo(User, { foreignKey: 'user_id', as: 'user' });
-
-// Idea-UserActions relationship: An idea can have many actions performed on it.
-Idea.hasMany(UserActions, { foreignKey: 'entity_id', scope: { entity_type: 'idea' }, as: 'ideaActions', constraints: false });
-UserActions.belongsTo(Idea, { foreignKey: 'entity_id', as: 'idea', constraints: false });
-
-// User-Idea (Users can have many ideas).
-User.hasMany(Idea, { foreignKey: 'user_id', as: 'ideas' });
-Idea.belongsTo(User, { foreignKey: 'user_id', as: 'user' });
-
-// Many-to-many relationship for users favoriting ideas.
-User.belongsToMany(Idea, { through: 'UserActions', as: 'favoritedIdeas', foreignKey: 'favorited_by', otherKey: 'entity_id', constraints: false });
-Idea.belongsToMany(User, { through: 'UserActions', as: 'favoritedByUsers', foreignKey: 'entity_id', otherKey: 'favorited_by', constraints: false });
-
-// Users commenting on ideas.
-User.hasMany(UserActions, { foreignKey: 'user_id', as: 'commentsOnIdeas', scope: { action_type: 'comment', entity_type: 'idea' } });
-UserActions.belongsTo(User, { foreignKey: 'user_id', as: 'commenter' });
-
-// Ideas having many comments.
-Idea.hasMany(UserActions, { foreignKey: 'entity_id', scope: { action_type: 'comment', entity_type: 'idea' }, as: 'ideaComments' });
-UserActions.belongsTo(Idea, { foreignKey: 'entity_id', as: 'commentedIdea', constraints: false });
-
-Idea.hasMany(UserActions, { foreignKey: 'entity_id', scope: { entity_type: 'idea' }, as: 'ideaactions' }); //getIdeaByUserId
-
-// Associations
-User.hasMany(Idea, { foreignKey: 'user_id' });
-Idea.belongsTo(User, { foreignKey: 'user_id' });
-
-User.hasMany(Like, { foreignKey: 'user_id' });
-Like.belongsTo(User, { foreignKey: 'user_id' });
-
-Idea.hasMany(Favorite, { foreignKey: 'idea_id' });
-Favorite.belongsTo(Idea, { foreignKey: 'idea_id' });
-
-User.hasMany(Favorite, { foreignKey: 'user_id' });
-Favorite.belongsTo(User, { foreignKey: 'user_id' });
-
-Idea.hasMany(Share, { foreignKey: 'idea_id' });
-Share.belongsTo(Idea, { foreignKey: 'idea_id' });
-
-User.hasMany(Share, { foreignKey: 'user_id' });
-Share.belongsTo(User, { foreignKey: 'user_id' });
-
-User.hasMany(UserActions, { foreignKey: 'user_id' });
-UserActions.belongsTo(User, { foreignKey: 'user_id' });
-
-Idea.hasMany(UserActions, { foreignKey: 'entity_id' });
-UserActions.belongsTo(Idea, { foreignKey: 'entity_id' });
-
-
-
 module.exports = {
   User,
   UserLog,
@@ -138,11 +70,7 @@ module.exports = {
   IssueComment,
   IssueHistory,
   IssueStats,
-  Idea,
   UserActions,
-  Idea,
   Like,
-  Favorite,
-  Share,
   UserConnection,
 };
--- src/Api/Models/InitializeDatabase.js
+++ src/Api/Models/InitializeDatabase.js
@@ -14,7 +14,6 @@ module.exports = {
         Permission.sync({ alter: false }),
         RolePermissions.sync({ alter: false }),
         ApplicationProperties.sync({ alter: false }),
-        Idea.sync({ alter: false }),
         UserActions.sync({ alter: false }),
         Like.sync({ alter: false }),
         Favorite.sync({ alter: false }),
@@ -23,7 +22,6 @@ module.exports = {
         IssueComment.sync({ alter: false }),
         IssueHistory.sync({ alter: false }),
         IssueStats.sync({ alter: false }),
-        Stats.sync({ alter: false }),
         UserConnection.sync({ alter: false }),
 
         BlockedUser.sync({ alter: false }),
